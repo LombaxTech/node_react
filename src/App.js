c1dcb2f7b8a7a51a56e72f564bc9108158ecaf5c
Rakib Khan <rakibkhan@live.co.uk>
2020-04-16 17:56:42 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [count, setCount] = useState(0);
+    const [secondCount, setSecondCount] = useState(10);
+
+    const increment = () => {
+        setCount(count + 1);
+    }
+
+    const incrementSecondCount = () => {
+        setSecondCount(secondCount + 10);
+    }
+
+    useEffect(() => {
+        document.title = `clicked ${count} times`
+        console.log('some change')
+    })
+
+    return (
+        <div>
+            <h2>Counter App</h2>
+            <h2>{count}</h2>
+            <button onClick={increment}>increment</button>
+            <h1>Second count: {secondCount}</h1>
+            <button onClick={incrementSecondCount}>Increment Second Count</button>
+        </div>
+    )
 }
+
+// function App() {
+//     return (
+//         <h2>Counter App</h2>
+//     );
+// }
+
+// class App extends Component {
+
+//     state = {
+//         count: 0
+//     }
+
+//     increment = () => {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+
+//     componentDidMount() {
+//         document.title = `Clicked ${this.state.count} times`;
+//     }
+
+//     componentDidUpdate() {
+//         document.title = `Clicked ${this.state.count} times`;
+//         // console.log('hello world')
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h2>Counter App!</h2>
+//                 <div>{this.state.count}</div>
+//                 <button onClick={this.increment}>Increment</button>
+//             </div>
+//         )
+//     }
+// }
 
 export default App;
