7b8744685c383cf81891a9c9fff358a9679d779d
Rakib Khan <rakibkhan@live.co.uk>
2020-04-16 19:24:57 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,32 +1,102 @@
 import React, { Component, useState, useEffect } from 'react';
 
+// const App = () => {
+//     const [count, setCount] = useState(0);
+//     const [secondCount, setSecondCount] = useState(10);
+
+//     const increment = () => {
+//         setCount(count + 1);
+//     }
+
+//     const incrementSecondCount = () => {
+//         setSecondCount(secondCount + 10);
+//     }
+
+//     useEffect(() => {
+//         document.title = `clicked ${count} times`
+//         console.log('some change')
+//     })
+
+//     return (
+//         <div>
+//             <h2>Counter App</h2>
+//             <h2>{count}</h2>
+//             <button onClick={increment}>increment</button>
+//             <h1>Second count: {secondCount}</h1>
+//             <button onClick={incrementSecondCount}>Increment Second Count</button>
+//         </div>
+//     )
+// }
+
 const App = () => {
-    const [count, setCount] = useState(0);
-    const [secondCount, setSecondCount] = useState(10);
+    // state
+    const [news, setNews] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('react');
+    const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+    const [loading, setLoading] = useState(false);
 
-    const increment = () => {
-        setCount(count + 1);
-    }
+    // fetch news
+    const fetchNews = async () => {
+        // setLoading(true)
+        // fetch('http://hn.algolia.com/api/v1/search?query=react')
+        // fetch(url)
+        //     .then(result => result.json())
+        //     // .then(data => console.log(data))
+        //     .then(data => setNews(data.hits), setLoading(false))
+        //     .catch(err => console.error(err))
 
-    const incrementSecondCount = () => {
-        setSecondCount(secondCount + 10);
+        setLoading(true);
+        try {
+            let result = await fetch(url);
+            let data = await result.json();
+            setNews(data.hits);
+            setLoading(false);
+        } catch (err) {
+            console.log(err)
+        }
+
     }
 
     useEffect(() => {
-        document.title = `clicked ${count} times`
-        console.log('some change')
-    })
+        fetchNews();
+    }, [url])
+
+    const handleChange = e => {
+        setSearchQuery(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    }
+
+    const showLoading = () => (loading ? <h2>Loading...</h2> : '');
+
+    const searchForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={searchQuery} onChange={handleChange} />
+                <button>Search</button>
+            </form>
+        )
+    }
+
+    const showNews = () => (
+        news.map((n, i) => (
+            <p index={i}>{n.title}</p>
+        ))
+    )
 
     return (
         <div>
-            <h2>Counter App</h2>
-            <h2>{count}</h2>
-            <button onClick={increment}>increment</button>
-            <h1>Second count: {secondCount}</h1>
-            <button onClick={incrementSecondCount}>Increment Second Count</button>
+            <h1>News</h1>
+            {showLoading()}
+            {searchForm()}
+            {showNews()}
         </div>
     )
 }
+
 
 // function App() {
 //     return (
